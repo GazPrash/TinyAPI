63ded341164987f1324c3b6e392d2c0aff63cc7c
GazPrash <prashantshrivastava118@gmail.com>
2024-08-27 14:34:20 +0000

    Added json data for examples

--- example1.cpp
+++ example1.cpp
@@ -1,13 +1,16 @@
-#include "include/tinyapi.h"
 #include "include/helper.h"
-#include<tuple>
+#include "include/tinyapi.h"
+#include <iostream>
+#include <ostream>
+#include <string>
+#include <tuple>
 
-
-// You can quickly define a function like this to convert images or any binary data into a string format.
-std::string readImageFile(const std::string& filename) {
+// You can quickly define a function like this to convert images or any binary
+// data into a string format.
+std::string readImageFile(const std::string &filename) {
   std::ifstream file(filename, std::ios::binary);
   if (!file) {
-      throw std::runtime_error("Failed to open the image file");
+    throw std::runtime_error("Failed to open the image file");
   }
   std::ostringstream ss;
   ss << file.rdbuf();
@@ -15,7 +18,7 @@ std::string readImageFile(const std::string& filename) {
 }
 
 // route = /home
-std::tuple<std::string, std::string> HomePage(){
+std::tuple<std::string, std::string> HomePage() {
   std::map<std::string, std::string> responseMap;
   responseMap["Greet"] = "Welcome Home!";
   auto response = Helper::MapToString(responseMap);
@@ -24,7 +27,7 @@ std::tuple<std::string, std::string> HomePage(){
 }
 
 // route = /about
-std::tuple<std::string, std::string> AboutPage(){
+std::tuple<std::string, std::string> AboutPage() {
   std::map<std::string, std::string> responseMap;
   responseMap["Made by"] = "Sammy Zane";
   responseMap["Total Downloads"] = "123,000+";
@@ -34,44 +37,59 @@ std::tuple<std::string, std::string> AboutPage(){
   return responseTup;
 }
 
-std::tuple<std::string, std::string> gatoImage(){
+std::tuple<std::string, std::string> gatoImage() {
   const std::string GATO_IMG_PATH = "images/gato.png";
   std::string imgContent = readImageFile(GATO_IMG_PATH);
   auto responseTup = std::make_tuple(imgContent, "image/png");
   return responseTup;
 }
 
-std::tuple<std::string, std::string> connector(std::string endpoint){
-  if (endpoint == "/home") return HomePage();
-  else if (endpoint == "/about") return AboutPage();
-  else if (endpoint == "/gato") return gatoImage();
-  // Create a custom 404 Message in case the user requests an unknown endpoint 
+std::tuple<std::string, std::string> getData() {
+  const std::string dataPath = "data/testdata.json";
+  auto response = Helper::jsonToString(dataPath);
+  auto responseTup = std::make_tuple(response, "text/html");
+  return responseTup;
+}
+
+std::tuple<std::string, std::string> connector(std::string endpoint) {
+  if (endpoint == "/home")
+    return HomePage();
+  else if (endpoint == "/about")
+    return AboutPage();
+  else if (endpoint == "/gato")
+    return gatoImage();
+  else if (endpoint == "/data")
+    return getData();
+  // Create a custom 404 Message in case the user requests an unknown endpoint
   // You must ensure that this function returns from all of its execution paths
   // so that no compilation error or undefined behaviour may happen.
   std::string err404 = "404 - Page Not Found!";
   return std::make_tuple(err404, "text/html");
 }
 
-int main(){
-  // Quickly setting up an HTTP server at device's localhost 
+int main() {
+  // Quickly setting up an HTTP server at device's localhost
   std::string localhost = "127.0.0.1";
-  TinyAPI *new_api = new TinyAPI(8000, 1024, 5, localhost, TinyAPI::HOST_OS::WIN);
-  if (new_api->initialize_server() == 1){
+  TinyAPI *new_api =
+      new TinyAPI(8000, 1024, 5, localhost, TinyAPI::HOST_OS::WIN);
+  /*std::cout << &new_api << std::endl;*/
+  if (new_api->initialize_server() == 1) {
     return 1;
   }
-  // this method activates a listening cycle so that a server can 
+  // this method activates a listening cycle so that a server can
   // recieve and respond to multiple client requests.
   // You must create a "connector" function that returns a string tuple
-  // The connector function must have all the logic regarding how a given request must be handled
-  // you can create different functions for handling different requests based on different routes
-  // so that you can tell TinyAPI what URL should trigger this function!
-  // Connector function must return a string tuple such that :
-  // First element is : text/json/binary in std::string format
-  // And the second element should be : format of the data i.e text/html or image/png or image/jpeg 
+  // The connector function must have all the logic regarding how a given
+  // request must be handled you can create different functions for handling
+  // different requests based on different routes so that you can tell TinyAPI
+  // what URL should trigger this function! Connector function must return a
+  // string tuple such that : First element is : text/json/binary in std::string
+  // format And the second element should be : format of the data i.e text/html
+  // or image/png or image/jpeg
   new_api->HttpRequestHandler(&connector);
   return 0;
 }
 
 // Compile as follows (Link with winsock2 / Windows only):
-// g++ -o example1_app.exe example1.cpp .\src\tinyapi.cpp -Iinclude -lws2_32  
+// g++ -o example1_app.exe example1.cpp .\src\tinyapi.cpp -Iinclude -lws2_32
 // And then run the executable file!
--- include/helper.h
+++ include/helper.h
@@ -1,45 +1,48 @@
 #ifndef HELPER_H
 #define HELPER_H
 
-#include<iostream>
-#include<string>
-#include<ctime>
-#include<csignal>
-#include<cstdlib>
-#include<fstream>
-#include<sstream>
-#include<map>
-#include<unordered_map>
+#include <csignal>
+#include <cstdlib>
+#include <ctime>
+#include <fstream>
+#include <iostream>
+#include <map>
+#include <sstream>
+#include <string>
+#include <unordered_map>
 
+namespace Helper {
+std::string MapToString(const std::map<std::string, std::string> dataMap) {
+  std::ostringstream map_string;
+  map_string << "{";
 
-namespace Helper{
-  std::string MapToString(const std::map<std::string, std::string> dataMap){
-    std::ostringstream map_string;
-    map_string << "{";
-
-    for (auto it = dataMap.begin(); it != dataMap.end(); ++it) {
-        if (it != dataMap.begin()) {
-            map_string << ", ";
-        }
-        map_string << "\"" << it->first << "\": \"" << it->second << "\"";
+  for (auto it = dataMap.begin(); it != dataMap.end(); ++it) {
+    if (it != dataMap.begin()) {
+      map_string << ", ";
     }
-    map_string << "}";
-    return map_string.str();
+    map_string << "\"" << it->first << "\": \"" << it->second << "\"";
   }
+  map_string << "}";
+  /*std::cout << "Map content as a string:\n" << map_string.str() <<
+   * std::endl;*/
+  return map_string.str();
+}
 
-  std::string jsonToString(const std::string filepath){
-    std::string jsonFilePath = "path_to_your_json_file.json";
-    std::ifstream jsonFile(filepath);
-    if (!jsonFile.is_open()) {
-        std::cerr << "Failed to open the JSON file." << std::endl;
-        return "";
-    }
-    std::string jsonString(
-      (std::istreambuf_iterator<char>(jsonFile)), std::istreambuf_iterator<char>()
-    );
-    std::cout << "JSON content as a string:\n" << jsonString << std::endl;
-    return jsonString;
+std::string jsonToString(const std::string filepath) {
+  std::string jsonFilePath = "path_to_your_json_file.json";
+  std::ifstream jsonFile(filepath);
+  if (!jsonFile.is_open()) {
+    std::cerr << "Failed to open the JSON file." << std::endl;
+    return "";
   }
-};
+  /*std::string jsonString((std::istreambuf_iterator<char>(jsonFile)),*/
+  /*                       std::istreambuf_iterator<char>());*/
+  std::ostringstream json_string;
+  json_string << jsonFile.rdbuf();
+  /*std::cout << "JSON content as a string:\n" << json_string.str() <<
+   * std::endl;*/
+  return json_string.str();
+}
+}; // namespace Helper
 
 #endif // HELPER_H
